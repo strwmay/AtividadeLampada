f2347bd509b59d96188dea70102771c2ed0ddb81
May <maysa.rondon@portalsesisp.org.br>
2024-03-26 16:29:23 -0300

    realização da atividade

--- AtividadeLampada/js/scripts.js
+++ AtividadeLampada/js/scripts.js
@@ -8,7 +8,7 @@ const acender = () => {
     // lampada.innerHTML = `<img id="lampada" src="img/acesa.jpg" alt="Lâmpada Acesa">`;
 
     imgLampada.src = "./img/acesa.jpg";
-    imgLampada.alt = "Lâmpada Acesa"
+    imgLampada.alt = "Lâmpada Acesa";
 
     btnEnviar.innerHTML = "Apagar";
     led = "apagar";
@@ -16,9 +16,27 @@ const acender = () => {
     // lampada.innerHTML = `<img id="lampada" src="img/apagada.jpg" alt="Lâmpada Acesa">`;
 
     imgLampada.src = "./img/apagada.jpg";
-    imgLampada.alt = "Lâmpada Apagada"
+    imgLampada.alt = "Lâmpada Apagada";
 
     btnEnviar.innerHTML = "Acender";
     led = "acender";
   }
+};
+
+// --------------------------------------------------------------------------- \\
+
+const gerarImagem = () => {
+  let qtdImagens = document.getElementById("inQtdImg").value; // valor informado pelo usuário
+  let canvas = document.getElementById("canvas");
+
+  canvas.innerHTML = "";
+  console.log(qtdImagens);
+
+  for (let i = 1; i <= qtdImagens; i++) {
+    canvas.innerHTML += `<img class="imagens" src="./img/logo.png">`;
+  }
+
+  // document.getElementById(
+  //   "canvas"
+  // ).innerHTML = `<img class="imagens" src="./img/logo.png" alt="logo do senai"></img>`;
 };
