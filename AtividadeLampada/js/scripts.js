eb870559cd11fa92a35b1b31a412499bdec560c1
May <maysa.rondon@portalsesisp.org.br>
2024-03-27 15:45:30 -0300

    finalização das atividades

--- AtividadeLampada/js/scripts.js
+++ AtividadeLampada/js/scripts.js
@@ -40,3 +40,77 @@ const gerarImagem = () => {
   //   "canvas"
   // ).innerHTML = `<img class="imagens" src="./img/logo.png" alt="logo do senai"></img>`;
 };
+
+// --------------------------------------------------------------------------- \\
+
+const validaForm = () => {
+  let data = document.getElementById("inData").value;
+  let cliente = document.getElementById("inCli").value;
+  let tel = document.getElementById("inFone").value;
+  let mail = document.getElementById("inMail").value;
+  let prod = document.getElementById("inProd").value;
+  let qtd = document.getElementById("inQtd").value;
+  let val = document.getElementById("inVal").value;
+  let msgErro = document.getElementById("mensagem-erro");
+
+  //apagando as informações de erro
+  msgErro.innerText = "";
+
+  //validação de dados
+  data == "" && (msgErro.innerHTML += "Preencha o campo 'Data'!<br>");
+  cliente == "" &&
+    (msgErro.innerHTML += "Preencha o campo 'Nome do cliente'! <br>");
+  cliente.length < 3 &&
+    (msgErro.innerHTML += "Campo 'Nome do cliente' incompleto! <br>");
+  tel == "" && (msgErro.innerHTML += "Preencha o campo 'Telefone'! <br>");
+  mail == "" && (msgErro.innerHTML += "Preencha o campo 'E-mail'! <br>");
+  mail.length < 5 && (msgErro.innerHTML += "Campo 'E-mail' incompleto! <br>");
+  prod == "" &&
+    (msgErro.innerHTML += "Preencha o campo 'Nome do produto'! <br>");
+  prod.length < 5 &&
+    (msgErro.innerHTML += "Campo 'Nome do produto' incompleto! <br>");
+  qtd == "" && (msgErro.innerHTML += "Preencha o campo 'Quantidade'! <br>");
+  qtd.length < 0 &&
+    (msgErro.innerHTML += "Número de campo 'Quantidade' inválido! <br>");
+  val == "" && (msgErro.innerHTML += "Preencha o campo 'Valor unitário'! <br>");
+  val.length < 0 &&
+    (msgErro.innerHTML += "Número de campo 'Valor unitário' inválido! <br>");
+
+  //mostrar e ocultar erro
+  msgErro.innerText == ""
+    ? ((msgErro.style.display = "none"), (msgErro.innerText = "enviado!"))
+    : (msgErro.style.display = "block");
+
+  //envio de formulário
+  msgErro.innerText == "enviado!" &&
+    ((msgErro.style.display = "block"),
+    (msgErro.innerText = "Formulário enviado com sucesso!"),
+    msgErro.classList.add("verde"));
+};
+
+// --------------------------------------------------------------------------- \\
+
+const calcular = () => {
+  let valPedido = document.getElementById("inValorPedido");
+  let perDesconto = document.getElementById("inPercDesc");
+  let valDesconto = document.getElementById("inValDesc");
+  let valFinal = document.getElementById("inValFinal");
+
+  switch (true) {
+    case valPedido.value >= 2000:
+      perDesconto.value = 1.5;
+      break;
+    case valPedido.value >= 1500:
+      perDesconto.value = 1;
+      break;
+    case valPedido.value >= 1000:
+      perDesconto.value = 0.8;
+      break;
+    case valPedido.value >= 500:
+      perDesconto.value = 0.5;
+      break;
+  }
+
+  valDesconto.value = valPedido.value * (perDesconto.value / 100);
+  valFinal.value = valPedido.value - valDesconto.value;
+};
